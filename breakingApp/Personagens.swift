f94863cdb71bf407620d0b273c411003deb47d49
Vicente Patricio <viceents@gmail.com>
2020-04-28 14:54:10 -0300

    fix prints

---
+++ breakingApp/Personagens.swift
@@ -0,0 +1,13 @@
+//
+//  Personagens.swift
+//  breakingApp
+//
+//  Created by Vicente Patricio on 27/04/20.
+//  Copyright © 2020 Vicente Patricio. All rights reserved.
+//
+
+import UIKit
+
+struct Personagens: Codable {
+    
+}
